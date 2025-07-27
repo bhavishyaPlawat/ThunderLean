87090d61a21815024944a9456a87af1e140c665b
Aniketh Singh <aniketh9410280466@gmail.com>
2025-07-27 22:22:55 +0530

    size changes

--- frontend/src/Components/Home.jsx
+++ frontend/src/Components/Home.jsx
@@ -7,32 +7,33 @@ import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div className="relative ">
+    <div className="relative">
+      {/* HERO SECTION */}
       <div
         className="bg-custom-gradient rounded-b-[13.88vw] pt-[2vh] px-[6vw] relative"
-        style={{ width: "1440px", height: "501px" }}
+        style={{ width: "100vw", height: "65vh" }}
       >
         {/* NAVBAR */}
         <Navbar />
 
-        {/* HERO SECTION */}
+        {/* HERO */}
         <Hero />
+      </div>
 
-        {/* BUTTON (Better Positioned) */}
-        <div className="absolute top-[50vh] left-[25vw] transform -translate-x-1/2 bottom-[-2rem]">
-          <Link
-            to="/dashboard"
-            className="w-[17.08vw] h-[5.97vh] bg-[#8C4DCF] text-white text-[1.3vw] font-semibold rounded-[1.39vw] flex items-center justify-center shadow-lg hover:bg-[#7a39c1] transition duration-300"
-          >
-            Start Now
-          </Link>
-        </div>
+      {/* START BUTTON (Centered between Hero & Features) */}
+      <div className="flex justify-center my-[6vh] absolute top-[45vh] left-[16vw]">
+        <Link
+          to="/dashboard"
+          className="w-[17vw] h-[6vh] bg-[#8C4DCF] text-white text-[1.3vw] font-semibold rounded-[1.5vw] flex items-center justify-center shadow-lg hover:bg-[#7a39c1] transition duration-300"
+        >
+          Start Now
+        </Link>
       </div>
 
       {/* FEATURES */}
       <Features />
 
-      {/* Footer */}
+      {/* FOOTER */}
       <PageFooter />
     </div>
   );
