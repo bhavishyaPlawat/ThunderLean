d297d130ddc496666b974f4a75f65745f6a33385
bhavishya plawat <tobhavishya2004@gmail.com>
2025-07-31 17:40:40 +0530

    update: cors added

--- Backend/app.js
+++ Backend/app.js
@@ -13,8 +13,8 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 // Middleware
-app.use(cors());
 app.use(helmet());
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
